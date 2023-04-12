1617c286a4aeb48987fbecca08801e00c2519569
ynrseong9 <ynr.seong9@gmail.com>
2023-04-12 13:04:17 +0900

    color button project

---
+++ color-button/src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+  const [disabled, setDisabled] = useState(false);
+
+  return (
+    <div>
+      <button
+        style={{ backgroundColor: buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
+      >
+        Change to {newButtonColor}
+      </button>
+      <input
+        type="checkbox"
+        id="enable-button-checkbox"
+        defaultChecked={disabled}
+        onClick={(e) => setDisabled(e.target.checked)}
+      />
+    </div>
+  );
+}
+
+export default App;
